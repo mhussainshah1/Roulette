8028a84e83c953b886ad28df97e44d42ad35e950
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-01 12:59:10 -0500

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,28 +24,27 @@ import java.util.Scanner;
  * In addition, also print out the number of games they have won and lost.
  */
 public class Main {
+    static int totalAmount, won, lost;
 
     public static void main(String[] args) {
-
         System.out.println("WELCOME! Let's play some Roulette!");
 
         Scanner keyboard = new Scanner(System.in);
         boolean choice = true;
-        int won = 0, lost = 0, totalAmount = 0;
 
+        String betInput, color = "";
+        int betAmountInput, betNumber, betColor;
         do {
             System.out.print("\nEnter the type of bet you would like to place (red|black|even|odd|high|low): ");
-            String betInput = keyboard.next().toLowerCase();
-
+            betInput = keyboard.next().toLowerCase();
             System.out.print("Enter in your bet amount: ");
-            int betAmountInput = keyboard.nextInt();
+            betAmountInput = keyboard.nextInt();
 
             // generate a number between 1 - 36
-            int betNumber = 1 + (int) (Math.random() * 36);
+            betNumber = 1 + (int) (Math.random() * 36);
 
             //generate two colors
-            String color = "";
-            int betColor = 1 + (int) (Math.random() * 2);
+            betColor = 1 + (int) (Math.random() * 2);
             switch (betColor) {
                 case 1:
                     color = "red";
@@ -55,97 +54,89 @@ public class Main {
                     break;
                 default:
                     System.out.println("Invalid Color");
-                    break; // break loop
-            }
+                    continue;            }
             System.out.println(" The ball landed on " + betNumber + " " + color + ".");
 
             //check red or black
             if (betInput.equals("red") || betInput.equals("black")) {
                 if (betInput.equals(color)) {
-//                win(betAmount);
-                    totalAmount += (betNumber * 2);
-                    won++;
-                    System.out.println("\tCongratulations, you've won.");
+                    win(betNumber);
                 } else {
-//                loose(betAmountInput);
-                    totalAmount -= betAmountInput;
-                    lost++;
-                    System.out.println("\tSorry, you've lost this bet.");
+                    loose(betAmountInput);
+                }
+            }
+            //check even or odd
+            else if (betInput.equals("even") || (betInput.equals("odd"))) {
+                if (betNumber % 2 == 0) {
+                    win(betNumber);
+                } else {
+                    loose(betAmountInput);
                 }
             }
             //check low or high
-            if (betInput.equals("low") || betInput.equals("high")) {
+            else if (betInput.equals("low") || betInput.equals("high")) {
                 if (betNumber <= 18) {
-                    //                win(betAmount);
-                    totalAmount += (betNumber * 2);
-                    won++;
-                    System.out.println("\tCongratulations, you've won.");
-
+                    win(betNumber);
                 } else {
-                    //                loose(betAmountInput);
-                    totalAmount -= betAmountInput;
-                    lost++;
-                    System.out.println("\tSorry, you've lost this bet.");
-
+                    loose(betAmountInput);
                 }
             }
-
-            double remainder;
-            if (betInput.equals("even") || (betInput.equals("odd"))) {
-                if (betNumber % 2 == 0) {
-                    //                win(betAmount);
-                    totalAmount += (betNumber * 2);
-                    won++;
-                    System.out.println("\tCongratulations, you've won.");
-                } else {
-                    //                loose(betAmountInput);
-                    totalAmount -= betAmountInput;
-                    lost++;
-                    System.out.println("\tSorry, you've lost this bet.");
-                }
+            else {
+                System.out.println("Invalid bet");
+                continue;
             }
+//            switch (betInput) {
+//                case "red": // Red: You are betting that the next roll will come up red.//
+//                case "black": //Black: You are betting that the next roll will come up black.
+//                    if (betInput.equals(color)) {
+//                        win(betNumber);
+//                    } else {
+//                        loose(betAmountInput);
+//                    }
+//                    break;
+//                case "even"://Even: This is wagering that the ball will land on an even number between 1-36.//
+//                case "odd"://Odd: This is wagering that the ball will land on an odd number between 1-36.
+//                    if (betNumber % 2 == 0) {
+//                        win(betNumber);
+//                    } else {
+//                        loose(betAmountInput);
+//                    }
+//                    break;
+//                case "low"://Low: This is a bet that the next number will be between 1 and 18.//
+//                case "high"://High: This is a bet that the next number will be between 19 and 36.
+//                    if (betInput.equals("low") || betInput.equals("high")) {
+//                        if (betNumber <= 18) {
+//                            win(betNumber);
+//                        } else {
+//                            loose(betAmountInput);
+//                        }
+//                    }
+//                    break;
+//                default:
+//                    System.out.println("Invalid bet");
+//                    continue;
+//            }
 
             System.out.println("\nYou currently have: $" + totalAmount);
             System.out.print("\nWould you like to play again (true|false)? ");
             choice = keyboard.nextBoolean();
         }
         while (choice);
+
         System.out.println("Thank you for playing!");
         System.out.println("\t This is how much money you have remaining: $" + totalAmount);
         System.out.println("You've won " + won + " game(s) and lost " + lost + " game(s).");
     }
 
-    public static void win(){
-
+    public static void win(int betNumber) {
+        totalAmount += (betNumber * 2);
+        won++;
+        System.out.println("\tCongratulations, you've won.");
     }
-    public static void loose(){
 
+    public static void loose(int betAmountInput) {
+        totalAmount -= betAmountInput;
+        lost++;
+        System.out.println("\tSorry, you've lost this bet.");
     }
 }
-
-//            String betType = "";
-//            switch(betInput){
-//                case "red": // Red: You are betting that the next roll will come up red.
-//                    betType = "red";
-//                    break;
-//                case "black": //Black: You are betting that the next roll will come up black.
-//                    betType = "black";
-//                    break;
-//                case "even"://Even: This is wagering that the ball will land on an even number between 1-36.
-//                    betType = "even";
-//                    break;
-//                case "odd":
-//                    betType = "odd";//Odd: This is wagering that the ball will land on an odd number between 1-36.
-//                    break;
-//                case "low":
-//                    betType = "low";//Low: This is a bet that the next number will be between 1 and 18.
-//                    int low = 1 + (int)(Math.random()* 18);
-//                    break;
-//                case "high":
-//                    betType = "high";//High: This is a bet that the next number will be between 19 and 36.
-//                    int high = 19 + (int)(Math.random()* 36);
-//                    break;
-//                default:
-//                    System.out.println("Invalid bet");
-//                    break; //break the loop.
-//            }
